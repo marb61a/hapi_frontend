873bcea00bb3529d4282fc08606544db628a2739
martin byrne <marb61a@gmail.com>
2016-09-21 10:37:43 +0000

    updated auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,37 @@ exports.register = function(server, options, next){
         method: 'POST',
         path: '/login',
         handler : function(request, reply){
+            const apiUrl = server.settings.app.apiBaseUrl + '/login/';
             
+            Wreck.post(apiUrl,{
+                payload : JSON.stringify(request.payload),
+                json : true
+            }, (err, res, payload) => {
+                if(err){
+                    throw err;
+                }
+                
+                if(res.statusCode !== 200){
+                    return reply.redirect('/login/');
+                }
+                
+                request.cookieAuth.set(payload);
+
+                return reply.redirect('/bookmarks');
+            });
+        }
+    });
+    
+    server.route({
+        method : 'GET',
+        path : '/logout',
+        handler : function(request, reply){
+            request.cookieAuth.clear();
+            
+            return reply.redirect('/bookmarks');
+        },
+        config : {
+            auth : 'session'
         }
     });
 };
